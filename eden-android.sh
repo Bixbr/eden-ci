e7af51e7896c698be1e9a63737d468d7bb6e2b2c
Bix <114880614+Bixbr@users.noreply.github.com>
2025-06-28 10:11:58 +0100

    Update eden-android.sh

--- eden-android.sh
+++ eden-android.sh
@@ -11,25 +11,29 @@ cd ./eden
 git submodule update --init --recursive
 
 # Workaround for prebuilt ffmpeg download failure
-# Use prebuilt ffmpeg from http://git.bixed.xyz/Bix/eden/
-# Content is unchanged
 sed -i 's|set(package_base_url "https://git.eden-emu.dev/eden-emu/")|set(package_base_url "http://git.bixed.xyz/Bix/eden/")|' CMakeModules/DownloadExternals.cmake
 sed -i 's|set(package_repo "ext-android-bin/raw/master/")|set(package_repo "raw/refs/heads/main/")|' CMakeModules/DownloadExternals.cmake
 
-# Optimised string.
+# Change app name suffix if TARGET is "Optimised"
 if [ "$TARGET" = "Optimised" ]; then
     sed -i 's/resValue("string", "app_name_suffixed", "eden")/resValue("string", "app_name_suffixed", "eden optimised")/' src/android/app/build.gradle.kts
-fi 
+fi
+
+# Count commits and set output APK name
 COUNT="$(git rev-list --count HEAD)"
 APK_NAME="Eden-${COUNT}-Android-Unofficial-${TARGET}"
 
 cd src/android
 chmod +x ./gradlew
+
+# Build APK based on target
 if [ "$TARGET" = "Optimised" ]; then
     ./gradlew assembleGenshinSpoofRelease --console=plain --info -Dorg.gradle.caching=true
 fi
 
+# Find and move the APK to the artifacts folder
 APK_PATH=$(find app/build/outputs/apk -type f -name "*.apk" | head -n 1)
+
 if [ -z "$APK_PATH" ]; then
     echo "Error: APK not found in expected directory."
     exit 1
