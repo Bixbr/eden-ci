7f125c203718c37cb246614b5e7c49c2edd9edef
Bix <114880614+Bixbr@users.noreply.github.com>
2025-07-26 17:35:29 +0100

    Update eden-android.sh

--- eden-android.sh
+++ eden-android.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ex
 
 # Clone Eden, fallback to mirror if upstream repo fails to clone
-if ! git clone 'https://git.eden-emu.dev/eden-emu/eden.git' ./eden; then
+if ! git clone -b 'release/0.0.3' 'https://git.eden-emu.dev/eden-emu/eden.git' ./eden; then
     echo "Using mirror instead..."
     rm -rf ./eden || true
-    git clone 'http://git.bixed.xyz/Bix/eden.git' ./eden
+    git clone 'http://,,,' ./eden
 fi
 
 cd ./eden
