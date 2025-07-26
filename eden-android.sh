5d1767cacefdfe9915eb5e979528fcbfdfc6081a
Bix <114880614+Bixbr@users.noreply.github.com>
2025-07-26 11:41:03 +0100

    Update eden-android.sh opimised package

--- eden-android.sh
+++ eden-android.sh
@@ -10,6 +10,8 @@ fi
 cd ./eden
 git submodule update --init --recursive
 
+sed -i 's/resValue("string", "app_name_suffixed", "eden")/resValue("string", "app_name_suffixed", "Eden Optimised")/' src/android/app/build.gradle.kts
+
 # Count commits and set output APK name
 COUNT="$(git rev-list --count HEAD)"
 APK_NAME="Eden-${COUNT}-Android-Unofficial-${TARGET}"
